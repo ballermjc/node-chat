8928a5486530533907ca4ff4a58c5acc2f349e83
Michael J Cooper <ballermjc@gmail.com>
2017-12-23 10:51:50 -0700

    chat server ready

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const   express     = require('express'),
+        bodyParser  = require('body-parser'),
+        app         = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../public/build'));
+
+const messagesController = require( __dirname + '/controllers/messages_controller');
+const baseURL = '/api/messages';
+
+app.get(baseURL, messagesController.read);
+app.post(baseURL, messagesController.create);
+app.put(`${baseURL}/:id`, messagesController.update);
+app.delete(`${baseURL}/:id`, messagesController.destroy);
+
+app.listen(3000, console.log("Boom Baby"));
